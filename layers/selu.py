182adda4162f707c486d780254a15343998ef710
Aditya Oke <47158509+oke-aditya@users.noreply.github.com>
2020-06-22 01:03:09 +0530

    reformatted with black:

--- layers/selu.py
+++ layers/selu.py
@@ -7,20 +7,21 @@ import torch
 from torch.nn import functional as F
 import torch.nn as nn
 
+
 def selu(x, inplace: bool = False):
     """ Selu as described in the paper Self-Normalizing Neural Networks https://arxiv.org/pdf/1706.02515.pdf """
     # Values taken from the paper
     alpha = 1.67326324
     scale = 1.05070098
     if inplace:
-        if(x < 0):
+        if x < 0:
             x.mul_(scale * alpha * (x.exp_() - 1))
         else:
             x.mul_(scale)
         return x
 
     else:
-        if(x < 0):
+        if x < 0:
             x.mul(scale * alpha * (x.exp() - 1))
         else:
             x.mul(scale)
@@ -31,9 +32,10 @@ class Selu(nn.Module):
     def __init__(self, inplace: bool = False):
         super().__init__()
         self.inplace = inplace
-    
+
     def forward(self, x):
-        return(selu(x, self.inplace))
+        return selu(x, self.inplace)
+
 
 # Sample code to check it
 
@@ -45,6 +47,3 @@ class Selu(nn.Module):
 
 #     x = torch.tensor(3, dtype=torch.float)
 #     print(nr2(x))
-
-
-
